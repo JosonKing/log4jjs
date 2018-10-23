d1514c933a685badf403f83988ca29eaaea3a5b1
josonking <wangsanmu5@163.com>
2018-10-23 16:15:44 +0800

    add log level

--- src/test.js
+++ src/test.js
@@ -1,3 +1,9 @@
-var { log, config } = require('./index');
-config('DEBUG', 'blue');
-log('hello log');
+var { config, log, info, error, warn, debug } = require('./index');
+
+// config();
+config('log', 'red', 'file');
+log('hello log', 'log2', 'log3');
+info('hello log');
+error('hello log');
+warn('hello log');
+debug('hello log');
